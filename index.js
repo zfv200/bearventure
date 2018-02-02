5791b65037d6c4b1e642a479a6088fe188f2224a
zfv200 <zach.vary@gmail.com>
2018-02-02 12:47:45 -0500

    Created game div and start button

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var canvas = document.getElementById("my-canvas");
 var ctx = canvas.getContext("2d");
+var bear = new Image();
+bear.src = ""
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+
+function addBear() {
+bear.addEventListener('load', function() {
+	ctx.drawImage(bear, 100, 400)
+	})
+}
+
+addBear();
+
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
